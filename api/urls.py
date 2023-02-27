21f40bdc8d75d32d58a79d88dd6a29480fc66943
Magdalena Slifierz <m.slifierz@gmail.com>
2023-02-26 22:01:33 -0800

    Git init

---
+++ api/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+# from .views import article_list, article_details
+# from .views import ArticleList, ArticleDetails
+from rest_framework.routers import DefaultRouter
+from .views import ArticleViewSet, UserViewSet
+
+router = DefaultRouter()
+router.register('articles', ArticleViewSet, basename='articles')
+router.register('users', UserViewSet)
+urlpatterns = [
+    path('api/', include(router.urls))
+    # path('articles/', ArticleList.as_view()),
+    # path('articles/<int:id>/', ArticleDetails.as_view())
+    # path('articles/', article_list),
+    # path('articles/<int:pk>', article_details),
+]
